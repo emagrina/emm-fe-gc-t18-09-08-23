a01ec39589284e46f9106f8ab7df5061726068f0
Ernest Magriñá <ernest8081@gmail.com>
2023-08-09 15:32:11 +0200

    chore: :tada: initial commit

---
+++ ex01/ex01/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleAplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!!!");
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ ex02/ex02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleAplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            double num3 = 2.57;
+
+            double suma = num1 + num3;
+            Console.WriteLine(suma);
+
+            Console.WriteLine("El numero introducido es: " + Convert.ToInt32(Console.ReadLine()));
+
+            Console.ReadKey();
+        }
+    }
+}
